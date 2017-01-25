c62c736df1134aac6e673c030da6f54a6af9e3bb
zengzheng <zengzheng2009@gmail.com>
2017-01-25 22:49:42 +0800

    implement CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :blogs
   root 'blogs#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118185942) do
+ActiveRecord::Schema.define(version: 20170119154018) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
