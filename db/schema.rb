a2935dff83888fc8987ab4a72dc3bb3b3da7d8ed
zengzheng <zengzheng2009@gmail.com>
2017-01-31 20:47:27 +0800

    change the title & destroy blog controller

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170126101633) do
 
-  create_table "blogs", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
