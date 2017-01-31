024c432715381224f61c8b12d34c6eb8c1367938
zengzheng <zengzheng2009@gmail.com>
2017-01-31 16:19:06 +0800

    check owner permission when access edit/update/destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
