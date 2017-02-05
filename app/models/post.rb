4c972bc01908dc046426e55a9c9ef9101bb3e13d
zengzheng <zengzheng2009@gmail.com>
2017-02-05 19:52:53 +0800

    category_name presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true
+  validates :category_name, presence: true
   scope :recent, -> {order("created_at DESC")}
 
   belongs_to :category
