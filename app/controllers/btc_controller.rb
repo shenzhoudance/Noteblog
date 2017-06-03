b63d3b481d8cac4bdf3d4dbb457ef80680905379
zengzheng <zengzheng2009@gmail.com>
2017-06-03 23:21:57 +0800

    add sc

--- app/controllers/btc_controller.rb
+++ app/controllers/btc_controller.rb
@@ -3,6 +3,7 @@ class BtcController < ApplicationController
   def index
     @usdt_btc = Poloniex::Market.by_pair("USDT_BTC").last.to_f * Money.default_bank.get_rate('USD', 'CNY').to_f
     @usdt_eth = Poloniex::Market.by_pair("USDT_ETH").last.to_f * Money.default_bank.get_rate('USD', 'CNY').to_f
+    @usdt_sc = Poloniex::Market.by_pair("BTC_SC").last.to_f * Poloniex::Market.by_pair("USDT_BTC").last.to_f * Money.default_bank.get_rate('USD', 'CNY').to_f
 
     @exchange_rate = Money.default_bank.get_rate('USD', 'CNY').to_f
 
@@ -10,5 +11,6 @@ class BtcController < ApplicationController
     response = client_public.get_public '/api/v2/tickers'
     @btc_yunbi = response["btccny"]["ticker"]["sell"]
     @eth_yunbi = response["ethcny"]["ticker"]["sell"]
+    @sc_yunbi = response["sccny"]["ticker"]["sell"]
   end
 end
