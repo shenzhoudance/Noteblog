d86433b4a795503b9c3f89412fb32c9bf6d6a3fe
zengzheng <zengzheng2009@gmail.com>
2017-06-18 14:32:43 +0800

    add column of pins id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617133548) do
+ActiveRecord::Schema.define(version: 20170618062504) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20170617133548) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
