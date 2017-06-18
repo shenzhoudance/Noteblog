8a731ce4972753c5f2fa887133510294921ee6e6
zengzheng <zengzheng2009@gmail.com>
2017-06-19 01:30:41 +0800

    add pin index/show

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,11 @@
 class PinsController < ApplicationController
- before_action :find_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
+  layout "pins"
+
   def index
-    @pins = Pin.all.order("created_at DESC")
+    @pins = Pin.all.order("created_at ASC")
   end
 
   def new
