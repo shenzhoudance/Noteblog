e2526e59a98b1cd1d4a542d274ceffcd4984a056
zengzheng <zengzheng2009@gmail.com>
2017-07-03 01:43:47 +0800

    jquery practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   namespace :skills do
     get 'front_end/work_1'
+    get 'js_practice/work'
     get 'js_practice/work_1'
+    get 'js_practice/work_2'
+    get 'js_practice/work_3'
+    get 'js_practice/work_4'
+    get 'js_practice/work_5'
   end
 
   devise_for :users
